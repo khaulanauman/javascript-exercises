e71005624af4082d6dabb901b03864b45c0362d1
khaulanauman <khaulanauman@gmail.com>
2024-03-09 14:47:26 +0500

    added code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...theArgs) {
+    let nArr=[];
+arr.forEach((a )=>
+{
+    if(!theArgs.includes(a)){
+        nArr.push(a);
+    }
+}
+)   
+return nArr;
 };
 
 // Do not edit below this line
