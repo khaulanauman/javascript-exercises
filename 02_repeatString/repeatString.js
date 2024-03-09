1686e467e755d175b3f983c8223fe82bb254bf86
khaulanauman <khaulanauman@gmail.com>
2024-03-09 11:28:41 +0500

    added loop for string repeatition

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+let rString="";
+for(let i=1;i<=num;i++){
+    rString+=string;
+}
+return rString;
 };
 
 // Do not edit below this line
